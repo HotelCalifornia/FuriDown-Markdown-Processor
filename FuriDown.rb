f171303bb205a642bb39f1af6f2c92ce01485e6c
Alex (a.k.a kintama.tar.gz) <ak.brooke@sbcglobal.net>
2015-10-14 11:16:59 -0500

    Update FuriDown.rb

--- FuriDown.rb
+++ FuriDown.rb
@@ -1,12 +1,15 @@
 require "redcarpet"
 
+# Redcarpet::Render::HTML is the full Redcarpet markdown processor (i.e. it parses markdown and returns HTML)
 class RubyMD < Redcarpet::Render::HTML
+  # Override the preprocess() hook to substitute the `[text|furigana]` tag with the HTML <ruby> tag (and its friends)
   def preprocess(doc)
     doc.gsub!(/\[(.+?)\|(.+?)\]/, "<ruby><rb>\\1</rb><rp>(</rp><rt>\\2</rt><rp>)</rp></ruby>")
     doc
   end
 end
 
+# The actual Jekyll plugin
 class Jekyll::Converters::Markdown::FuriDown
 	def initialize(config)
 		@config = config
